abb3891a29c0c88d5b3af7541f513dd754063a72
Katarína Kalusová <kallusova.katka@gmail.com>
2020-11-18 09:58:47 +0100

    database connection added

---
+++ db_connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$mysqli = new mysqli("localhost","c1kalusova","amHqWX9oBTWc","c1kalusova");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+
+$mysqli -> set_charset("utf8");
+?>
